bed9a79261bda743adff5eb64c160da9de9464fd
PMServices <PMServices@optimationtech.local>
2019-04-17 08:57:43 -0400

    working as of 4/17

---
+++ pylogix/examples/collect_data.bat
@@ -0,0 +1,34 @@
+@echo off
+set arg1=%1
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-1 
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-2
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-3
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-4
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-5
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-6
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-7
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-8
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-9
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-10
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-11
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-12
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-13
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-14
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-15
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-16
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-17
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-18
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-19
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-20
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-21
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-22
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-23
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-24
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-25
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-26
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-27
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-28
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-29
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-30
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-31
+start /B python log_samples_rate_tags.py 5000 %arg1% chunk-32
---
+++ pylogix/examples/log_samples_rate_tags.py
@@ -0,0 +1,51 @@
+'''
+the following import is only necessary because eip.py is not in this directory
+'''
+import sys
+import os
+sys.path.append('..')
+samples = sys.argv[1]
+samples = int(samples)
+#rate = sys.argv[2]
+#rate = float(rate)
+testname = sys.argv[2]
+filename = sys.argv[3]
+
+dirpath = os.getcwd()
+filepath = dirpath + '\{}'.format(filename)
+with open(filepath, encoding='utf-16') as f:
+    taglist = f.read()
+    tags = '[' + taglist.strip() + ']'
+
+'''
+We're going to log a few tag values 10
+times to a CSV file
+'''
+
+import csv
+from pylogix import PLC
+import time
+from datetime import datetime
+import ast
+
+with PLC() as comm:
+    comm.IPAddress = '192.168.113.30'
+
+    tags = ast.literal_eval(tags)
+    print(filename)
+
+    now = datetime.now()
+    now = now.strftime('%d%m%Y%H%M%S')
+    with open(str(testname) + '_' + str(filename) + '_' + now + '{}'.format('.csv'), 'w') as csv_file:
+        csv_file = csv.writer(csv_file, delimiter=',', lineterminator='\n', quotechar='/', quoting=csv.QUOTE_MINIMAL)
+        header = tags[:] 
+        header[:0] = ['utc']
+        csv_file.writerow(header)
+        for i in range(samples):
+            now = datetime.now()
+            now = now.strftime('%Y-%m-%dT%H:%M:%S.%f')
+            values = comm.Read(tags)
+            values = ", ".join( repr(e) for e in values)
+            csv_file.writerow([now, values])
+
+            # time.sleep(rate)
